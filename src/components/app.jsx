e74c9a0dad31332e7405c0d24aab443e075adcd6
Ankur <ankur3176@gmail.com>
2022-06-04 18:01:13 +0530

    secondcomit

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Header from "./header";
 import Footer from "./footer";
+import Note from "./note";
 function App(){
     return <div><Header />
+    <Note />
     <Footer /></div>
 }
 
---
+++ src/components/note.jsx
@@ -0,0 +1,10 @@
+import react from "react";
+
+function Note(){
+    return <div className="note">
+        <h1>My first react app</h1>
+        <p>This is my first react app and i'm loving the way react js works </p>
+    </div>
+}
+
+export default Note;
